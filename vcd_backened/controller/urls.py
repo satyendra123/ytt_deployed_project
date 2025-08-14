09d6f387caba313ef3ac3c9665d8258786d0844b
satyendra123 <Singhsatyendra885@gmail.com>
2025-08-14 09:58:00 +0530

    Delete vcd_backened directory

--- vcd_backened/controller/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import check_vehicle_data, check_boom
-
-urlpatterns = [
-    path('vehicle_data', check_vehicle_data, name='vehicle_data'),
-    path('check_boomsig', check_boom, name='check_boomsig'),
-]
